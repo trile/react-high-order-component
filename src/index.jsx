734982a134b98a9d2b59a5d37c92af2e2dbb1d17
Tri Le <trile@users.noreply.github.com>
2017-07-13 10:11:34 +0700

    Inital commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloReact from './components/hello_react';
+
+ReactDOM.render(
+  <HelloReact />,
+  document.getElementById('root');
+)
