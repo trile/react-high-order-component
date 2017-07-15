64eab0517734faee6ca606c8999b6a9d9ec22e0e
Tri Le <trile@users.noreply.github.com>
2017-07-15 16:32:59 +0700

    add babel-stage-0

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015']
+        presets: ['react', 'es2015', 'stage-0']
       },
       test: /\.jsx?$/
     }]
